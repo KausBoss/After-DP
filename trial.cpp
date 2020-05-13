678f49d3ba7f0dab8aee00b8527d048e29a30cc6
KausBoss <ktkaustubh4@gmail.com>
2020-05-14 01:11:30 +0530

    1

--- trial.cpp
+++ trial.cpp
@@ -1,7 +1,16 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+#define ll long long
 
 int main(){
-	cout<<"Hello world";
+ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int n;
+    cin>>n;
+    cout<<n*n;
 }
